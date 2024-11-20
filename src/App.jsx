c25e8f640e64ddc1cc8799458a922998a452ec91
Martin Hernandez <lmhr79@gmail.com>
2024-11-20 12:03:55 -0300

    ultimas modificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar/Navbar";
-import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"; // Importación única
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"; 
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import NoPage from "./components/NoPage/NoPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<ItemListContainer />} />
         <Route path="/category/:id" element={<ItemListContainer />} /> 
         <Route path="/detail/:id" element={<ItemDetailContainer />} /> 
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/NoPage/NoPage.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import "./NoPage.css"; // Archivo CSS para estilos
+
+function NoPage() {
+  return (
+    <div className="not-found-container">
+      <h1 className="not-found-title">404 - Página No Encontrada</h1>
+      <p className="not-found-message">
+        Lo sentimos, la página que buscas no existe o ha sido movida.
+      </p>
+      <Link to="/" className="not-found-link">
+        Volver al Inicio
+      </Link>
+    </div>
+  );
+}
+
+export default NoPage;
