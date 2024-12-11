5d478fdbbd6e2a5846b33b4eba5a76e52b363593
Martin Hernandez <lmhr79@gmail.com>
2024-12-11 00:50:44 -0300

    Asegurar que Navbar.css esté incluido

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
 import CartWidget from "../CartWidget/CartWidget";
-import "./navbar.css";
+import "./Navbar.css";
 
 function Navbar() {
   const location = useLocation();
